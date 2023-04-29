fb6ac382c62670b36ca09e46f364f94e3e0b65ad
William David <98925022+widacasan@users.noreply.github.com>
2023-04-28 21:45:50 -0500

    Revert "Create page login"

---
+++ src/components/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+import './styles.css';
+
+function LoginPage() {
+  return (
+    <div className="login">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;
