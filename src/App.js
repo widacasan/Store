9e0dab830a64c978e198454e55dae024dc63665d
William Camacho <widacasan@gmail.com>
2023-05-09 21:27:37 -0500

    add interacction from LoginPage to Home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
-import "./components/styles.css";
+import TopBar from './components/TopBar'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div>
-      
-      <LoginForm/>
-
-    </div>
-  );
-}
+   
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoginForm />
+          </Route>
+          <Route path="/topbar">
+            <TopBar />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+  
 
 export default App;
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,55 +1,68 @@
 import React, { useState } from "react";
 import faviconYardSale from "../assets/logos/favicon_yard_sale.svg";
-import "./App.css";
+import "./LoginFrom.css";
+import { useHistory } from "react-router-dom";
 
 function LoginForm() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    const user = { 
+      userName: 'nombre',
+      password: '123'
+    }
+    if(user.userName === email && user.password === password ){
+      history.push("/topbar");
+    }
+    console.log({email,password})
   };
 
+
   return (
-    <div className="form-container">
-      <img src={faviconYardSale} alt="logo" />
+    <div className="container">
+  <div className="form-container">
+    <img src={faviconYardSale} alt="logo" />
 
-      <form class="form" onSubmit={handleSubmit}>
-        <label htmlFor="email" class="label">
-          Email address
-        </label>
-        <input
-          type="text"
-          id="email"
-          placeholder="platzi@example.com"
-          class="input input-email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
+    <form className="form" onSubmit={handleSubmit}>
+      <label htmlFor="email" className="label">
+        Email address
+      </label>
+      <input
+        type="text"
+        id="email"
+        placeholder="nombre@example.com"
+        className="input input-email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
 
-        <label htmlFor="password" class="label">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          placeholder="*********"
-          class="input input-password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+      <label htmlFor="password" className="label">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        placeholder="*********"
+        className="input input-password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
 
-        <input
-          type="submit"
-          value="Log in"
-          class="primary-button login-button"
-        />
+      <input
+        type="submit"
+        value="Log in"
+        className="primary-button login-button"
+      />
 
-        <a href="/">Forgot my password</a>
-      </form>
+      <a href="/">Forgot my password</a>
+    </form>
 
-      <button class="secondary-button signup-button">Sign up</button>
-    </div>
+    <button className="secondary-button signup-button">Sign up</button>
+  </div>
+</div>
   );
 }
 
